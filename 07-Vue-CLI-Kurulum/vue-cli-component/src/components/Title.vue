075b4f9489839d831aebf7a6fae5c78eb6d4be9b
omeralitosun <omerttosu@gmail.com>
2023-01-12 19:27:28 +0300

    vue cli kurulumu

---
+++ 07-Vue-CLI-Kurulum/vue-cli-component/src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>{{ title }}</h3>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title:"Vue 3'e Giriş"
+        }
+    },
+}
+</script>
+
+<style scoped>
+ h3{
+    color: orangered;
+    font-size: 30px;
+    font-weight: 700;
+ }
+</style>
---
+++ 07-Vue-CLI-Kurulum/vue-cli/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h3>{{ title }}</h3>
+  <input type="text" ref="name"><br><br>
+  <input type="text" ref="password"><br><br>
+  <button @click="login">Giriş</button>
+  <br>
+  <p v-show="isLogin">Giriş Başarılı</p>
+  <p v-show="!isLogin">Giriş Başarısız</p>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: "Login",
+      name:"Ömer",
+      password:"123456",
+      isLogin:null
+    }
+  },
+  methods: {
+    login(){
+      return this.$refs.name.value === this.name && this.$refs.password.value === this.password ? this.isLogin=true : this.isLogin=false
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
